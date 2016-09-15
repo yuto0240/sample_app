5864cac89bd0d5f0750f3e0dad8f9414e25d2ad7
インターン <yuto0240@kta.biglobe.ne.jp>
2016-09-15 15:24:46 +0900

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   #root 'static_pages#home'
   get 'static_pages/home'
-  
+
   get 'static_pages/help'
   get 'static_pages/about'
-  get  'static_pages/contact'
+  get 'static_pages/contact'
 
 
   get 'signup'  => 'users#new'
